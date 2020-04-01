44c047593b193cd9898c732b2b1027e7c8ccd3f3
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-04-01 15:37:50 +0200

    feature sender completed

--- clinet.c
+++ clinet.c
@@ -7,16 +7,19 @@
 #include"messeging.h"
 
 #define SH_BUFFER_SIZE 128
-
+char *sender;
 int main(int argc, char *argv[]){
 	key_t key = 12345;
 	int shmid = shmget(key, SH_BUFFER_SIZE, 0666);
 	ERRCHECKER_shared_memory_getting(shmid);
 	char *shm = shmat(shmid, NULL , 0);
 	ERRCHECKER_shared_memory_atteching(shm);
-
+	if (argc == 1)
+		sender = "server";
+	else if (argc == 2)
+		sender = argv[1];
 	reciving(shm);
-	send_messege(argv[1],"resp", shm);
+	send_messege(sender,"resp", shm);
 	reciving(shm);
 
 	return 0;
--- server.c
+++ server.c
@@ -7,17 +7,20 @@
 #include"messeging.h"
 
 #define SH_BUFFER_SIZE 128
-
+char *sender;
 int main(int argc, char *argv[]){
 	key_t key = 12345;
 	int shmid = shmget(key,SH_BUFFER_SIZE,IPC_CREAT | 0666);
 	ERRCHECKER_shared_memory_getting(shmid);
 	char *shm = shmat(shmid,NULL,0);
 	ERRCHECKER_shared_memory_atteching(shm);
-
-	send_messege(argv[1],"what??",shm);
+	if (argc == 1)
+		sender = "server";
+	else if (argc == 2)
+		sender = argv[1];
+	send_messege(sender,"what??",shm);
 	reciving(shm);
-	send_messege(argv[1],"woooo",shm);
+	send_messege(sender,"woooo",shm);
 	return 0;
 }
 
